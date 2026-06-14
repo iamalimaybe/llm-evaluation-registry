7d138d4ff896bb5e4c7615a69ba75bc530317ab2
iamalimaybe <52281468+iamalimaybe@users.noreply.github.com>
2026-06-14 11:26:21 +0500

    Initialize Spring Boot project

---
+++ src/main/java/com/aliniaz/llmeval/LlmEvaluationRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.aliniaz.llmeval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmEvaluationRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmEvaluationRegistryApplication.class, args);
+	}
+
+}
